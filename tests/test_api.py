4f1bb19bda89c4760ef7022ab70de86b5be2f6f3
peag <s214987@student.dtu.dk>
2025-01-19 14:48:29 +0100

    Finished API test.

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,3 +1,4 @@
+import logging
 from fastapi.testclient import TestClient
 
 from src.models.predict_model import app
@@ -6,11 +7,11 @@ from src.models.predict_model import app
 def test_api_request_valid():
     client = TestClient(app)
     response = client.get("/translate/The house is wonderful")
-    assert response.status_code == 200
+    assert response.status_code == 200, "API request failed."
     assert response.json() == {
         "en": "The house is wonderful",
         "de translation": "Das Haus ist wunderbar.",
-    }
+    }, "API response is not as expected." 
 
 
 def test_api_request_empty_str():
